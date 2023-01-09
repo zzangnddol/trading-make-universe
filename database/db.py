8dfe7d78d492de37cc8bce63ee8294f83efd8da1
송영배 <ybsong@inzent.com>
2023-01-09 10:38:35 +0900

    connection pool을 사용하도록 함

--- database/db.py
+++ database/db.py
@@ -1,5 +1,8 @@
+from abc import ABC
+
 from peewee import Model
 from playhouse.mysql_ext import MariaDBConnectorDatabase
+from playhouse.pool import PooledDatabase
 
 ip = 'myplan.ddns.net'
 port = 3306
@@ -7,7 +10,18 @@ database = 'system_trading'
 user = 'ybsong'
 password = 'Thddudqo072!'
 
-db = MariaDBConnectorDatabase(database, host=ip, port=port, user=user, password=password, autoconnect=True)
+
+class PooledMariaDBDatabase(PooledDatabase, MariaDBConnectorDatabase, ABC):
+    def _is_closed(self, conn):
+        try:
+            conn.ping(False)
+        except (Exception,):
+            return True
+        else:
+            return False
+
+
+db = PooledMariaDBDatabase("system_trading", host=ip, port=port, user=user, password=password, autoconnect=True, stale_timeout=300)
 
 
 class BaseModel(Model):
