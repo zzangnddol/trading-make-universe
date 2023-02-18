34b1ee26820a4a43499618aa94af962caaadd1bd
송영배 <ybsong@inzent.com>
2023-02-18 12:20:56 +0900

    #7 10 거래일 내 25% 이상 하락날이 있는 경우 유니버스 대상에서 제거

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import schedule
 from database.db import db
 from database.model.account_models import AccountBalance
 from database.model.process_models import write_process_status, ProcessStatus
-from database.model.stock_models import StockInfo, StockPrice
+from database.model.stock_models import StockInfo, StockPrice, get_stock_prices_dataframe
 from database.model.strategy_models import UniverseTest
 from util.notify import Notifier
 
@@ -45,17 +45,27 @@ def make_universe(strategy_id=1):
 @db.atomic()
 def __make_universe(strategy_id=1, without_insert=False) -> int:
     print("유니버스 생성 작업 시작")
+    # 이전 유니버스 삭제
     UniverseTest.delete().where(UniverseTest.stragegy_id == strategy_id).execute()
 
+    # 보유 종목 조회
     balances = __get_account_balances()  # 보유 종목
     suspended_codes = []  # 거래정지 종목 코드 목록
 
+    # 유니버스 만들기
     count = 0
     stock: StockInfo
     for stock in StockInfo.select():
-        last_stock_price: StockPrice = __get_stock_last_price_data(stock.code)
-        if last_stock_price is None:
+        stock_prices_10d = get_stock_prices_dataframe(stock.code).iloc[-10:]
+        if len(stock_prices_10d) <= 1:
             continue
+
+        # 10거래일 내 25% 이상 하락한 날이 하루 이상인 종목 제거
+        days_down_25 = len(stock_prices_10d.loc[(stock_prices_10d['change'] < -0.25)])  # 25% 이상 하락한 날 수
+        if days_down_25 > 0:
+            logger.info(f"------ 10 거래일 내 하루 25%이상 하락한 종목: {stock.code} {stock.name}, 하락일 수: {days_down_25}")
+            continue
+        last_stock_price: StockPrice = stock_prices_10d.iloc[-1]
 
         # 거래 정지 상태
         if last_stock_price.open == 0:
@@ -155,3 +165,9 @@ def __start_make_universe(is_test=False, without_insert=False):
 if __name__ == '__main__':
     __start_make_universe()  # 실 사용
     # __start_make_universe(is_test=True, without_insert=True)  # 테스트
+
+    # prices = get_stock_prices_dataframe('000020')
+    # print(len(prices))
+    # prices_10d: DataFrame = prices.iloc[-10:]
+    # print(prices_10d)
+    # print(prices_10d.loc[(prices_10d['change'] < -0.25)])
